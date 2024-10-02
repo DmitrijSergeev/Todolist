9ee5d555862e4c8108126757ef64de4feeb4176d
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-10-02 17:22:49 +0400

    store

---
+++ src/store/task-reducer/task-reducer.tsx
@@ -0,0 +1,9 @@
+export const taskReducer = (state: any, action: any) => {
+    switch (action.type) {
+        case '': {
+            return state
+        }
+        default:
+            return state
+    }
+}
